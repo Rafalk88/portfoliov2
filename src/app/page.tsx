1ddb072cfd7c8eb1c9083870ee209489d0117d01
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2024-11-28 21:44:34 +0100

    feat: add figure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[180px_minmax(0,1fr)] items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
+    <section className="grid grid-rows-[180px_minmax(0,1fr)] items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
       <header className="">
         <Image
           className="h-[180px] object-cover"
@@ -12,9 +12,21 @@ export default function Home() {
           height={180}
         />
       </header>
-      <main className="">
-        footer
+      <main className="relative max-w-[75%] xl:max-w-[50%] w-full h-full">
+        <figure className="absolute top-[-10%]">
+          <Image
+            className="rounded-full w-28 h-28 mb-4"
+            src="/assets/avatar.webp"
+            alt="Zdjęcie profilowe Rafał Kochanecki"
+            width={112}
+            height={112}
+          />
+          <figcaption>
+            <h3 className="text-4xl font-bold">Rafał Kochanecki</h3>
+            <h4 className="font-bold text-gray-500">Frontend developer</h4>
+          </figcaption>
+        </figure>
       </main>
-    </div>
+    </section>
   );
 }
