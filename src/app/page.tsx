f9f37d10ec322352cfa4b6f6b9ca0508a1d43c43
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2024-11-28 21:05:21 +0100

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[180px_minmax(0,1fr)] items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
+      <main className="">
+        main
+      </main>
+      <footer className="">
+        footer
+      </footer>
+    </div>
+  );
+}
