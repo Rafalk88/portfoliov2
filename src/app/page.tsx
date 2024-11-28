6f99049c33d30bbb2f6632d397a609cd57e930d1
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2024-11-28 21:16:38 +0100

    css: add Image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[180px_minmax(0,1fr)] items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
+      <header className="">
+        <Image
+          className="h-[180px] object-cover"
+          src="/assets/space.jpg"
+          alt="space_image"
+          width={1920}
+          height={180}
+        />
+      </header>
       <main className="">
-        main
-      </main>
-      <footer className="">
         footer
-      </footer>
+      </main>
     </div>
   );
 }
